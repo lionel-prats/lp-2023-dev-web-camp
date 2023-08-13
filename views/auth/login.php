e09f3f38fd6c261d6a3890b18a987581667bb01c
lionel-prats <lionel.prats.c@gmail.com>
2023-08-13 20:05:03 -0300

    SECCION 78 - VIDEO 672

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,1 +1,31 @@
-<h1><?php echo $titulo; ?></h1>
+<main class="auth">
+    <h2 class="auth__heading"><?php echo $titulo; ?></h2>
+    <p class="auth__texto">Inicia sesión en DevWebcamp</p>
+    <form action="" class="formulario" novalidate>
+        <div class="formulario__campo">
+            <label for="email" class="formulario__label">Email</label>
+            <input 
+                type="email"
+                class="formulario__input"
+                placeholder="Tu email"
+                id="email"
+                name="email"
+            >
+        </div>
+        <div class="formulario__campo">
+            <label for="password" class="formulario__label">Password</label>
+            <input 
+                type="password"
+                class="formulario__input"
+                placeholder="Tu password"
+                id="password"
+                name="password"
+            >
+        </div>
+        <input type="submit" value="Iniciar Sesión" class="formulario__submit">
+    </form>
+    <div class="acciones">
+        <a href="/registro" class="acciones__enlace">¿Aún no tienes cuenta? Obtener una</a>
+        <a href="/registro" class="acciones__enlace">¿Olvidaste tu password?</a>
+    </div>
+</main>
