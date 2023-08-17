53e24f39d517dfb3d99c6e990d3b295d718bcea3
lionel-prats <lionel.prats.c@gmail.com>
2023-08-17 00:22:46 -0300

    SECCION 81 - VIDEO 692

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,7 +1,10 @@
 <main class="auth">
     <h2 class="auth__heading"><?php echo $titulo; ?></h2>
     <p class="auth__texto">Inicia sesión en DevWebcamp</p>
-    <form action="" class="formulario" novalidate>
+
+    <?php require_once __DIR__ . "/../templates/alertas.php"; ?>
+    
+    <form method="POST" class="formulario" novalidate>
         <div class="formulario__campo">
             <label for="email" class="formulario__label">Email</label>
             <input 
