dc8a163ba69ad06acfe9c1b628409ca7595a25a7
lionel-prats <lionel.prats.c@gmail.com>
2023-08-18 00:11:58 -0300

    SECCION 82 - VIDEO 695

---
+++ views/templates/admin-sidebar.php
@@ -0,0 +1,34 @@
+<aside class="dashboard__sidebar">
+    <nav class="dashboard__menu">
+        <a href="/admin/dashboard" class="dashboard__enlace">
+            <i class="fa-solid fa-house"></i><!-- house -->
+            <span class="dashboard__menu-texto">
+                Inicio
+            </span>
+        </a>
+        <a href="/admin/ponentes" class="dashboard__enlace">
+            <i class="fa-solid fa-microphone"></i><!-- microphone -->
+            <span class="dashboard__menu-texto">
+                Ponentes
+            </span>
+        </a>
+        <a href="/admin/eventos" class="dashboard__enlace">
+            <i class="fa-solid fa-calendar"></i></i><!-- calendar -->
+            <span class="dashboard__menu-texto">
+                Eventos
+            </span>
+        </a>
+        <a href="/admin/registrados" class="dashboard__enlace">
+            <i class="fa-solid fa-users"></i><!-- users -->
+            <span class="dashboard__menu-texto">
+                Registrados
+            </span>
+        </a>
+        <a href="/admin/registrados" class="dashboard__enlace">
+            <i class="fa-solid fa-gift"></i><!-- gift -->
+            <span class="dashboard__menu-texto">
+                Regalos
+            </span>
+        </a>
+    </nav>
+</aside>
