478f07701ae43eb670b33e30b16920530bb0a69f
lionel-prats <lionel.prats.c@gmail.com>
2023-08-19 18:40:55 -0300

    SECCION 83 - VIDEO 700

--- views/admin/ponentes/crear.php
+++ views/admin/ponentes/crear.php
@@ -9,3 +9,24 @@
         Volver
     </a>
 </div>
+
+<div class="dashboard__formulario">
+    <?php 
+        # __DIR__ = "C:\xampp\htdocs\curso-valdez-php\lp-2023-dev-web-camp\views\admin\ponentes"
+        include_once __DIR__ . "./../../templates/alertas.php";
+    ?>
+
+    <!-- enctype="multipart/form-data" -> atributo HTML para poder subir archivos (VIDEO 700) -->
+    <form 
+        method="POST" 
+        class="formulario"
+        enctype="multipart/form-data"     
+    >
+        <?php include_once __DIR__ . "./formulario.php"; ?>
+        <input 
+            type="submit" 
+            value="Registrar Ponente"
+            class="formulario__submit"    
+        >
+    </form>
+</div>
