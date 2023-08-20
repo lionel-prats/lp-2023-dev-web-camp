773a6618d2fd0e0c8efbcf7f66e6a7194a9c9f62
lionel-prats <lionel.prats.c@gmail.com>
2023-08-19 23:33:43 -0300

    SECCION 85 - VIDEO 707

--- controllers/PonentesController.php
+++ controllers/PonentesController.php
@@ -3,6 +3,7 @@
 namespace Controllers;
 
 use MVC\Router;
+use Model\Ponente;
 
 class PonentesController {
     public static function index(Router $router) {
@@ -13,10 +14,18 @@ class PonentesController {
     public static function crear(Router $router) {
 
         $alertas = [];
+        $ponente = new Ponente;
+
+        if($_SERVER["REQUEST_METHOD"] === "POST") {
+            $ponente->sincronizar($_POST);
+            $alertas = $ponente->validar();
+            
+        }
 
         $router->render("admin/ponentes/crear", [
             "titulo" => "Registrar Ponente",
-            "alertas" => $alertas
+            "alertas" => $alertas,
+            "ponente" => $ponente
         ]);
     }
 }
