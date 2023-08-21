a4a12b0df50819e8547651d6d99115d1fc693666
lionel-prats <lionel.prats.c@gmail.com>
2023-08-20 21:30:00 -0300

    SECCION 86 - VIDEO 711

--- views/admin/ponentes/index.php
+++ views/admin/ponentes/index.php
@@ -13,7 +13,7 @@
 <div class="dashboard__contenedor">
     <?php if (!empty($ponentes)): ?>
         <table class="table">
-            <thead>
+            <thead class="table__thead">
                 <tr>
                     <th scope="col" class="table__th">Nombre</th>
                     <th scope="col" class="table__th">Ubicación</th>
@@ -30,12 +30,18 @@
                             <?php echo $ponente->ciudad . ", " . $ponente->pais; ?>
                         </td>
                         <td class="table__td--acciones">
-                            <a href="/admin/ponentes/editar?id=<?php echo $ponente->id; ?>">
+                            <a  
+                                href="/admin/ponentes/editar?id=<?php echo $ponente->id; ?>"
+                                class="table__accion table__accion--editar"    
+                            >
                                 <i class="fa-solid fa-user-pen"></i>
                                 Editar
                             </a>
                             <form action="" method="" class="table__formulario">
-                                <button type="submit">
+                                <button 
+                                    type="submit"
+                                    class="table__accion table__accion--eliminar"    
+                                >
                                     <i class="fa-solid fa-circle-xmark"></i>    
                                     Eliminar
                                 </button>
