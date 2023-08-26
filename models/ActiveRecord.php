5afa60f3daad3c04ed3c948bb7099edfec37b47d
lionel-prats <lionel.prats.c@gmail.com>
2023-08-26 19:13:41 -0300

    SECCION 88 - VIDEO 737

--- models/ActiveRecord.php
+++ models/ActiveRecord.php
@@ -148,6 +148,20 @@ class ActiveRecord {
         $resultado = self::consultarSQL($query);
         return array_shift( $resultado );
     }
+
+    // 
+    public static function whereArray($array = []) {
+        $query = "SELECT * FROM " . static::$tabla . " WHERE ";
+        foreach($array as $key => $value) {
+            if($key === array_key_last($array)) {
+                $query .= "$key = '$value'";
+            } else {
+                $query .= "$key = '$value' AND ";
+            }
+        }
+        $resultado = self::consultarSQL($query);
+        return array_shift( $resultado );
+    }
     
     // Retorna el total de registros de una tabla
     public static function total() : int {
