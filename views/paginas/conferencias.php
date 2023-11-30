ddf9a9505140c132acdf8ef906faecc333ce14c4
lionel-prats <lionel.prats.c@gmail.com>
2023-11-30 01:16:43 -0300

    SECCION 90 - VIDEO 763

--- views/paginas/conferencias.php
+++ views/paginas/conferencias.php
@@ -7,34 +7,38 @@
         
         <p class="eventos__fecha">Viernes 5 de Octubre</p>
 
-        <div class="eventos__listado">
-            <?php foreach($eventos["conferencias_v"] as $evento):?>
-                <div class="evento">
-                    <p class="evento__hora"><?php echo $evento->hora->hora; ?></p>
-                    <div class="evento__informacion">
-                        <h4 class="evento__nombre"><?php echo $evento->nombre; ?></h4>
-                        <p class="evento__introduccion">
-                            <?php echo $evento->descripcion; ?>
-                        </p>
-                        <div class="evento__autor-info">
-                            <picture class="evento__imagen-autor">
-                                <source srcset="/img/speakers/<?php echo $evento->ponente->imagen; ?>.webp" type="image/webp">
-                                <source srcset="/img/speakers/<?php echo $evento->ponente->imagen; ?>.png" type="image/png">
-                                <img 
-                                    src="/img/speakers/<?php echo $evento->ponente->imagen; ?>.png" 
-                                    alt="Imagen Ponente"
-                                    loading="lazy"
-                                    width="200"
-                                    height="300"
-                                >
-                            </picture>
-                            <p class="evento__autor-nombre">
-                                <?php echo $evento->ponente->nombre . " " . $evento->ponente->apellido; ?>
+        <div class="eventos__listado slider swiper">
+            <div class="swiper-wrapper">
+                <?php foreach($eventos["conferencias_v"] as $evento):?>
+                    <div class="evento swiper-slide">
+                        <p class="evento__hora"><?php echo $evento->hora->hora; ?></p>
+                        <div class="evento__informacion">
+                            <h4 class="evento__nombre"><?php echo $evento->nombre; ?></h4>
+                            <p class="evento__introduccion">
+                                <?php echo $evento->descripcion; ?>
                             </p>
+                            <div class="evento__autor-info">
+                                <picture class="evento__imagen-autor">
+                                    <source srcset="/img/speakers/<?php echo $evento->ponente->imagen; ?>.webp" type="image/webp">
+                                    <source srcset="/img/speakers/<?php echo $evento->ponente->imagen; ?>.png" type="image/png">
+                                    <img 
+                                        src="/img/speakers/<?php echo $evento->ponente->imagen; ?>.png" 
+                                        alt="Imagen Ponente"
+                                        loading="lazy"
+                                        width="200"
+                                        height="300"
+                                    >
+                                </picture>
+                                <p class="evento__autor-nombre">
+                                    <?php echo $evento->ponente->nombre . " " . $evento->ponente->apellido; ?>
+                                </p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            <?php endforeach;?>
+                <?php endforeach;?>
+            </div>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
         </div>
 
         <p class="eventos__fecha">Sábado 6 de Octubre</p>
